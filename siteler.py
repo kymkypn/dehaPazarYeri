aaead912e59bc6381fc5d124e39c5f5af4967963
kymkypn <eksot1@hotmail.com>
2018-07-17 16:41:48 +0300

    Add files via upload

---
+++ siteler.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+class gitti():
+    def siparisler(self):
+        self.gonderilenler(text="Giriş yok")
+    def gonderilenler(self):
+        Label(text="Giriş yok")
+    def iadeler(self):
+        Label(text="Giriş yok")
+
+    def mesajlar(self):
+        Label(text="Giriş yok")
+
+    def urunler(self):
+        Label(text="Giriş yok")
+
+
+class n11_magaza1():
+    def siparisler(self):
+        Label(text="Giriş yok")
+    def gonderilenler(self):
+        Label(text="Giriş yok")
+    def iadeler(self):
+        Label(text="Giriş yok")
+
+    def mesajlar(self):
+        Label(text="Giriş yok")
+
+    def urunler(self):
+        Label(text="Giriş yok")
+
+
+class n11_magaza2():
+    def siparisler(self):
+        Label(text="Giriş yok")
+    def gonderilenler(self):
+        Label(text="Giriş yok")
+    def iadeler(self):
+        Label(text="Giriş yok")
+
+    def mesajlar(self):
+        Label(text="Giriş yok")
+
+    def urunler(self):
+        Label(text="Giriş yok")
+
+
+class hb():
+    def siparisler(self):
+        Label(text="Giriş yok")
+    def gonderilenler(self):
+        Label(text="Giriş yok")
+    def iadeler(self):
+        Label(text="Giriş yok")
+
+    def mesajlar(self):
+        Label(text="Giriş yok")
+
+    def urunler(self):
+        Label(text="Giriş yok")
+
+
+
+
+
